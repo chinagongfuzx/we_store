b1dbb8a7a932185116af65c4e4aa4d5b5abfb649
Coco <1947606351@qq.com>
2019-09-25 16:21:40 +0800

    合并更改

--- src/router.js
+++ src/router.js
@@ -5,7 +5,6 @@ import Picdetail from './components/photo_sharing/Picdetail.vue'
 import login from './components/login.vue'
 import news from './components/news/news.vue'
 import NewsDetails from './components/news/NewsDetails.vue'
-import photo_sharing from './components/photo_sharing/photo_sharing.vue'
 import goods from './components/goods/goods.vue'
 Vue.use(Router)
 
