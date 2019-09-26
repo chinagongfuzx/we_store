81dd9f48c09a727092ec48cc893aea08bd66f468
Owen <512808529@qq.com>
2019-09-26 12:06:22 +0800

    完成了vip页面

--- src/components/vip/vip.vue
+++ src/components/vip/vip.vue
@@ -1,12 +1,35 @@
 <template>
-    <div></div>
+  <div class="vipBox">
+    <van-icon name="setting-o" />
+    <p>正在加班加点开发中，敬请期待！</p>
+  </div>
 </template>
+
 <script>
-export default {
-    
-}
+export default {}
 </script>
+
 <style lang="less" scoped>
+@keyframes setting {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(1turn);
+  }
+}
 
+.vipBox {
+  text-align: center;
+
+  .van-icon {
+    margin-top: 20px;
+    font-size: 25px;
+    animation: setting 2s linear infinite;
+  }
+
+  p {
+    margin: 10px 0;
+  }
+}
 </style>
-
