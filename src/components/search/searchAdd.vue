ddc999d02b6b8c58a87891ce6911dff69ea32e1f
Owen <512808529@qq.com>
2019-09-26 12:50:47 +0800

    合并了搜索页面添加商品功能

--- src/components/search/searchAdd.vue
+++ src/components/search/searchAdd.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <div style="padding: 0 10px">
     <van-cell-group>
-      <van-field v-model="value" placeholder="请输入商品名称"/>
+      <van-field v-model="value" placeholder="请输入商品名称" />
     </van-cell-group>
     <van-button type="info" @click="addSearch">添加商品</van-button>
   </div>
@@ -11,18 +11,37 @@ import addSearchList from '@/api'
 export default {
   data() {
     return {
-      value: ""
-    };
+      value: ''
+    }
   },
   methods: {
-   async addSearch() {
-     const {data:res}=  await addSearchList(this.value)
-     if(res.status !== 0) return this.$Notify({ type: 'danger', message: '添加失败' })
-     this.$Notify({ type: 'success', message: '添加成功' });
+    async addSearch() {
+      if (!this.value) {
+        return this.$notify({
+          message: '请输入商品名称',
+          duration: 1000
+        })
+      } else if (!this.value.trim()) {
+        this.value = ''
+        return this.$notify({
+          message: '商品名称不能为空',
+          duration: 1000
+        })
+      }
+      const { data: res } = await addSearchList(this.value)
+      if (res.status !== 0) return this.$Notify({ type: 'danger', message: '添加失败' })
+      this.$Notify({ type: 'success', message: '添加成功' })
     }
   }
-};
+}
 </script>
 <style lang="less" scoped>
+.van-button {
+  width: 100%;
+  height: 40px;
+  margin: 40px 0;
+  font-size: 16px;
+  line-height: 40px;
+}
 </style>
 
