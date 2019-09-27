330bfa6faaddd1d64f079e50c318ee530b41441b
Owen <512808529@qq.com>
2019-09-27 12:01:58 +0800

    修改了配置文件的代码格式

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,16 @@
 const productionPlugins = [] // 发布阶段用到的babel插件
 if (process.env.NODE_ENV === 'production') {
-
-	// 如果项目在生产阶段，则使用 babel-plugin-transform-remove-console 去页面中的console
-	productionPlugins.push('transform-remove-console')
+  // 如果项目在生产阶段，则使用 babel-plugin-transform-remove-console 去页面中的console
+  productionPlugins.push('transform-remove-console')
 }
 
 module.exports = {
-	presets: [
-		'@vue/app'
-	],
-	plugins: [
-
-		// 使用 @babel/plugin-syntax-dynamic-import 插件实现路由懒加载
-		'@babel/plugin-syntax-dynamic-import',
-		...productionPlugins // 解构数组，开发阶段得到空，生产阶段得到'transform-remove-console'
-	]
+  presets: [
+    '@vue/app'
+  ],
+  plugins: [
+    // 使用 @babel/plugin-syntax-dynamic-import 插件实现路由懒加载
+    '@babel/plugin-syntax-dynamic-import',
+    ...productionPlugins // 解构数组，开发阶段得到空，生产阶段得到'transform-remove-console'
+  ]
 }
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-	devServer: {
-		port: '8281'
-		// open: true
-	}
+  devServer: {
+    port: '8281'
+  }
 }
